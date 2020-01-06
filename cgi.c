186cf140fed1b4453b52dca53b8bb19201b555b6
ganqingbo <18244969528@163.com>
2020-01-06 06:40:39 -0800

    create cgi,c

---
+++ cgi.c
@@ -0,0 +1,13 @@
+#include "weblet.h"
+int main(void){
+    char *buf,*p;
+    char content[MAXLINE];
+    int n1=0,n2=0;
+    scanf("%d&%d",&n1,&n2);
+    sprintf(content,"%ss%d+%d=%d\r\n",content,n1,n2,n1+n2);
+    printf("Content-length:%d\r\n",strlen(content));
+    printf("Content-type:text/html\r\n\r\n");
+    printf("%s",content);
+    fflush(stdout);
+    exit(0);
+}
