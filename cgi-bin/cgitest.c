a04c57cfbc92a15e1271c100d634cedd093abc0c
ganqingbo <18244969528@163.com>
2020-01-06 00:35:42 -0800

    new folder

---
+++ cgi-bin/cgitest.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+        char *buf,*ptr;
+        char context[MAXLINE];
+        int i,n=0;
+        double sum=1;
+        scanf("%d",&n);
+        for(i=1;i<n;i++){
+            sum=sum*i;
+        }
+        sprintf(content,"CGI TEST:");
+        sprintf(content,"%s%d的阶乘是:%lf\r\n",content,n,sum);
+        printf("ContentLength:%d\r\n",strlen(content));
+        printf("ContentType:text/html\r\n\r\n");
+        printf("%s",content);
+        fflush(stdout);
+        exit(0);
+
+}
